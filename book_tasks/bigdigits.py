25cb1526d4eb152e409dc301ba871840fb2a77cd
Alexandr Damakalshchikov <a.damakalshchikov@gmail.com>
2019-01-26 21:11:43 +0300

    Digits prints by *

---
+++ book_tasks/bigdigits.py
@@ -0,0 +1,88 @@
+import sys
+
+Zero  = [" *** ", 
+         "*   *", 
+         "*   *", 
+         "*   *",
+         "*   *",
+         "*   *",
+         " *** "]
+		 
+One   = [" * ",
+         "** ",
+         " * ",
+         " * ",
+         " * ",
+         " * ",
+         "***"]
+		 
+Two   = [" *** ",
+         "*   *",
+         "*  * ",
+         "  *  ",
+         " *   ",
+         "*    ",
+         "*****"]
+Three = [" *** ",
+         "*   *",
+         "    *",
+         "  ** ",
+         "    *",
+         "*   *",
+         " *** "]
+Four  = ["    * ",
+         "   ** ",
+         "  * * ",
+         " *  * ",
+         "******",
+         "    * ",
+         "    * "]
+Five  = ["*****",
+         "*    ",
+         "*    ",
+         " *** ",
+         "    *",
+         "*   *",
+         " *** "]
+Six   = [" *** ",
+         "*   *",
+         "*    ",
+         "**** ",
+         "*   *",
+         "*   *",
+         " *** "]
+Seven = ["*****",
+         "    *",
+         "   * ",
+         "  *  ",
+         " *   ",
+         "*    ",
+         "*    "]
+Eight = [" *** ",
+         "*   *",
+         "*   *",
+         " *** ",
+         "*   *",
+         "*   *",
+         " *** "]
+Nine  = [" ****",
+         "*   *",
+         "*   *",
+         " ****",
+         "    *",
+         "    *",
+         " *** "]
+
+Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+		 
+numbers = sys.argv[1]
+
+try:
+    for row in range(7):
+        one_string = str()
+        for num in numbers:
+            one_string += Digits[int(num)][row] + " "
+        print(one_string)
+except ValueError as incorrect_arg:
+	print(incorrect_arg)
+

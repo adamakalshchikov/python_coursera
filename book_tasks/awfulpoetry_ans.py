cdc4daa5b85859fbc1322f49c67cb08f609c4852
Alexandr Damakalshchikov <a.damakalshchikov@gmail.com>
2019-01-27 01:26:02 +0300

    Available setting number of strings

--- book_tasks/awfulpoetry_ans.py
+++ book_tasks/awfulpoetry_ans.py
@@ -1,3 +1,4 @@
+import sys
 import random
 
 articles = ['the', 'a']
@@ -5,11 +6,19 @@ subjects = ['cat', 'dog', 'man', 'woman']
 verbs = ['sang', 'rang', 'jumped']
 adverbs = ['loudly', 'quitley', 'well', 'badly']
 
+
 def print_a_string(first_word, second_word, third_word):
     print(random.choice(first_word) + ' ' + random.choice(second_word) + ' ' +
                   random.choice(third_word))
 
-for _ in range(5):
+
+try:
+    number_of_strings = int(sys.argv[1])
+except IndexError:
+    number_of_strings = 5
+
+
+for _ in range(number_of_strings):
     if random.randint(0, 1):
         print_a_string(articles, subjects, verbs)
     else:

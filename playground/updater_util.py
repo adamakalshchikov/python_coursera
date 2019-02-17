ca46180de1d922bbcd2dd41e1a8d5d9cea91527a
Alexandr Damakalshchikov <a.damakalshchikov@gmail.com>
2019-02-18 00:14:23 +0300

    Start defining of container copying

--- playground/updater_util.py
+++ playground/updater_util.py
@@ -5,7 +5,7 @@ import os
 from shutil import copyfile
 
 
-FW_PATH = r'C:\FirmwareUpdater\settings.json'
+FW_PATH = r'C:\FirmwareUpdater'
 MAIN_PATH = r'\\cri-files\Build\Platforn2.5'
 BRANCH_PATH = r'\branch'
 
@@ -17,7 +17,7 @@ def fw_params():
 	parser.add_argument('-br', '--branch', action='store_true', default=False)
 	parser.add_argument('-p', '--port', type=int, default=None)
 	parser.add_argument('-r','--release', action='store_true', default=False,
-						help='Either to update to build`s release version')
+						help='Either to load release version')
 	args = parser.parse_args()
 	return args
 
@@ -27,17 +27,27 @@ def con_path(build: int, is_branch: bool):
 	parent_dir = Path(MAIN_PATH)
 	if not is_branch:
 		temp = parent_dir / 'container'
-		for dir in temp.iterdir():
-			if build in dir:
-				build_dir = build
+	else:
+		branch = input("Specify the branch, where container built: ")
+		temp = parent_dir / f'{branch}' / 'container'
+	for directory in temp.iterdir():
+		if build in directory:
+			build_dir = build
 	assert build_dir is not None
 	return build_dir
 
 
+def file_copier(build_dir:str, is_release: bool, build: int, model: int, dst = FW_PATH):
+
+
+
 upd_options = fw_params()
 
-with open('settings.json', 'r+') as f:
+with open(os.path.join(FW_PATH, 'settings.json'), 'r+') as f:
 	settings = json.load(f)
 	settings['com']['number'] = upd_options.port
 	json.dump(settings, f)
 	f.close()
+
+
+os.system(f'FirmwareUpdater.exe -f "{container_name}"')

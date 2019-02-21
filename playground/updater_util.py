05867e4ae015d4098c9b4d09bd9652cb39dba935
Alexandr Damakalshchikov <a.damakalshchikov@gmail.com>
2019-02-21 23:53:25 +0300

    Found decision for many directories
     - Added docstring to file_copier function;
     - Function which returned single build directory now return dict of directories;
     - Start to define function, which provide choosing amount directories wich meet`s with
    required build number.

--- playground/updater_util.py
+++ playground/updater_util.py
@@ -1,8 +1,10 @@
+from pathlib import Path
+from shutil import copyfile
+from itertools import count
 import argparse
 import json
-from pathlib import Path
 import os
-from shutil import copyfile
+
 
 
 FW_PATH = r'C:\FirmwareUpdater'
@@ -22,9 +24,12 @@ def fw_params():
 	return args
 
 
-def con_path(build: int, is_branch: bool):
-	build_dir = None
-	dir_candidate = None
+def get_candidates(build: int, is_branch: bool):
+	"""Функция принимает на вход номер билда и сведения о брачне, возвращает словарь с директориями,
+	содержащими в названии номер билда."""
+
+	build_dirs = {}
+	number = count(start=1)
 	parent_dir = Path(MAIN_PATH)
 	if not is_branch:
 		temp = parent_dir / 'container'
@@ -33,15 +38,27 @@ def con_path(build: int, is_branch: bool):
 		temp = parent_dir / f'{branch}' / 'container'
 	for directory in temp.iterdir():
 		if build in directory:
-			build_dir = build
-	assert build_dir is not None
-	return build_dir
+			build_dirs[number] = build
+	assert build_dirs is not None
+	return build_dirs
+
+
+def
 
 
 def file_copier(build_dir: Path, is_release: bool, build: int, model: str, dst = FW_PATH):
+	"""Функция копирует контейнер с прошивкой в папку FirmwareUpdater
+
+	build_dir - корневая директория билда, содержащая папки 'debug' и 'release';
+	is_release - если True - выбирается папка 'release';
+	build - номер билда, используется для исключения ошибочного выбора папки;
+	model - модель прошиваемого оразца ККТ;
+	dst - папка, в которую будет скопирован контейнер
+	 """
+
 	dir = build_dir / 'release' if is_release else build_dir / 'debug'
 	for d in dir.iterdir():
-
+		# выбор папки по модели ККТ
 
 
 upd_options = fw_params()

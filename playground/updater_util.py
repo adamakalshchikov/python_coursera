278c5cd8295a9ea142bbeab3fced125594d1c93d
Дамакальщиков Александр Александрович <a.damakalshchikov@atol.ru>
2019-03-11 13:56:01 +0300

    Утилита работает!

--- playground/updater_util.py
+++ playground/updater_util.py
@@ -6,7 +6,7 @@ import sys
 import os
 
 FW_PATH = r'C:\FirmwareUpdater'
-MAIN_PATH = r'\\cri-files\Build\Platforn2.5'
+MAIN_PATH = r'\\cri-files\Builds$\FRPlatform2.5'
 BRANCH_PATH = r'\branch'
 
 
@@ -14,10 +14,11 @@ def fw_params():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-m', '--model', type=str, default=None)
 	parser.add_argument('-b', '--build', type=int, default=None)
-	parser.add_argument('-br', '--branch', action='store_true', default=False)
-	parser.add_argument('-p', '--port', type=int, default=None)
+	parser.add_argument('-br', '--branch', action='store_true', default=False,
+						help='Установите данный флаг, если прошивка была собрана в бранче. Далее потребуется ввести название бранча.')
+	parser.add_argument('-p', '--port', type=int, default=None, help='Порт, к которому подключена ККТ.')
 	parser.add_argument('-r', '--release', action='store_true', default=False,
-						help='Установите данный флаг, если требуется установить релизную версию прошивки')
+						help='Установите данный флаг, если требуется установить релизную версию прошивки. По умолчанию будет установлена отладочная версия.')
 	args = parser.parse_args()
 	return args
 
@@ -32,11 +33,15 @@ def get_candidates(build: int, is_branch: bool):
 		temp = parent_dir / 'container'
 	else:
 		branch = input("Укажите бранч, в котором находится контейнер: ")
-		temp = parent_dir / f'{branch}' / 'container'
+		temp = parent_dir / 'branch' / f'{branch}' / 'container'
 	for directory in temp.iterdir():
 		if str(build) in directory.name:
 			build_dirs.append(directory)
-	assert build_dirs is not None
+	try:
+		assert len(build_dirs) >= 1
+	except AssertionError:
+		print('Не найдена указанная версия прошивки. Введите номер прошивки, существующий в папке ...\Platforma2.5\container\,либо укажите бранч, в котором находится требуемый билд')
+		sys.exit(0)
 	return build_dirs
 
 
@@ -47,7 +52,7 @@ def choose_dir(directories_candidates: list):
 
 	if len(directories_candidates) > 1:
 		candidates = dict()
-		msg = f'Введите номер директории (от 1 до {0})'
+		msg = 'Выберете папку с прошивкой (от 1 до {0}): '
 		print('Выберете требуемый каталог:')
 		for elem in enumerate(directories_candidates, start=1):
 			print(f'{elem[0]}. {elem[1]}')
@@ -85,7 +90,7 @@ def file_copier(build_dir: Path, is_release: bool, build: int, model: str):
 		file.close()
 	con_name = None
 	for child in directory_of_container.iterdir():
-		if str(build) in child:
+		if str(build) in child.name:
 			copyfile(src=str(child), dst=str(Path(FW_PATH) / child.name))
 			con_name = child.name
 	return con_name
@@ -93,16 +98,21 @@ def file_copier(build_dir: Path, is_release: bool, build: int, model: str):
 
 if __name__ == '__main__':
 	upd_options = fw_params()
-
+	
 	# Пишем номер порта в settings.json
-	with open(os.path.join(FW_PATH, 'settings.json'), 'r+') as file_settings:
+	with open(os.path.join(FW_PATH, 'settings.json'), 'r') as file_settings:
 		settings = json.load(file_settings)
+		file_settings.close()
+	with open(os.path.join(FW_PATH, 'settings.json'), 'w') as file_settings:
 		settings['com']['number'] = upd_options.port
 		json.dump(settings, file_settings)
 		file_settings.close()
+		
 
-	list_of_directories = get_candidates(upd_options.build, upd_options.release)
+
+	list_of_directories = get_candidates(upd_options.build, upd_options.branch)
 	dir_with_container = choose_dir(list_of_directories)
 	container_name = file_copier(dir_with_container, upd_options.release, upd_options.build, upd_options.model)
 	print(f'FirmwareUpdater.exe -f "{container_name}"')  # использовать для отладки
-# os.system(f'FirmwareUpdater.exe -f "{container_name}"')
+	#os.system('cd C:\FirmwareUpdater')
+	#os.system(f'C:\FirmwareUpdater\FirmwareUpdater.exe -f "{container_name}"')

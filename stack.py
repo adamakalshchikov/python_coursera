68f44cf1ca22711ed45a48109ee4e7bdbdaab1a6
Alexandr Damakalshchikov <a.damakalshchikov@gmail.com>
2019-06-10 12:49:43 +0300

    Initial version

--- stack.py
+++ stack.py
@@ -2,8 +2,8 @@
 
 
 def check_brackets(case):
-	direct = ['(', '[', '{']
-	reverse = [')', ']', '}']
+	direct = {'(', '[', '{'}git 
+	reverse = {')', ']', '}'}
 
 	brackets_d = dict()
 	brackets_r = dict()
@@ -14,19 +14,20 @@ def check_brackets(case):
 	s = list()
 	n = list()
 	for num, i in enumerate(case, start=1):
-		try:
-			if i == brackets_d[s[-1]]:
-				s.pop()
-				n.pop()
-			else:
+		if i in direct.union(reverse):
+			try:
+				if i == brackets_d[s[-1]]:
+					s.pop()
+					n.pop()
+				else:
+					s.append(i)
+					n.append(num)
+			except IndexError:
 				s.append(i)
 				n.append(num)
-		except IndexError:
-			s.append(i)
-			n.append(num)
-		except KeyError:
-			pass
+			except KeyError:
+				pass
 	return (s, n)
 
 
-print(check_brackets('{{[()]]'))
+print(check_brackets('([](){([])})'))
